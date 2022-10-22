c1aecebfc3ba42dd79975a60a8e44c431f8f7635
Sayakie <sayakie@kakao.com>
2022-10-22 15:43:25 +0900

    style: use single quote instead backtick

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import './config.js'
 import 'zx/globals'
 
-import { Client } from './lib/client/Client.js'
+import { Client } from '#lib/client/Client.js'
 
-$.verbose = process.env.NODE_ENV !== `production`
+$.verbose = process.env.NODE_ENV !== 'production'
 
 const client = new Client()
-await client.login().catch(reason => {
-  process.exitCode = 1
 
-  client.logger.error(reason)
-  client.destroy()
-})
+await client
+  .login() //
+  .catch(reason => {
+    process.exitCode = 1
+
+    client.logger.error(reason)
+    client.destroy()
+  })
